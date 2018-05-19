452372b8901e072be21b33ae7b8295deac3335d9
Román SG <roman.sosa@atos.net>
2018-05-19 16:27:32 +0100

    Add missing header license

--- tab-deploy/flow-pusher.js
+++ tab-deploy/flow-pusher.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2018 Atos, and others
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License 2.0
+* which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-2.0/
+*
+* Contributors:
+* Atos - initial implementation
+*******************************************************************************/
 var request=require('request');
 var fs = require('fs');
 var d = require('debug')('flow-pusher')
