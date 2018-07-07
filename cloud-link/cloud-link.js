d60bb86724234791a348ad52aa500b7884eb07a9
Román SG <roman.sosa@atos.net>
2018-07-07 20:39:59 +0100

    Make small changes to facilitate benchmarking

--- cloud-link/cloud-link.js
+++ cloud-link/cloud-link.js
@@ -10,9 +10,10 @@
 *******************************************************************************/
 module.exports = function(RED) {
     //use this module to make requests
-    var d = require('debug')('cloud-link')
-    var request=require('request');
-    var exec = require('child_process').exec;
+    const d = require('debug')('cloud-link')
+    const request=require('request');
+    const os = require('os');
+    const perf = require('./perf-values')
 
     function CloudLink(config) {
         RED.nodes.createNode(this,config);
@@ -24,19 +25,29 @@ module.exports = function(RED) {
             d("cloud-link.on input");
 
             getValues(config.dummy, msg).then( result => {
-
-                if (toBeSentRemote(config, result)) {
+                var thresholds = msg.payload.thresholds ||
+                    {
+                        "mem": config.mem, "cpu": config.cpu, "temp": config.temperature
+                    };
+                msg.payload.thresholds = thresholds;
+                msg.payload.values = result;
+                if (msg.link == "remote" || toBeSentRemote(thresholds, result)) {
                     url = config.remote;
                     msg.payload.remote = true;
                     var options = {
                         method: "POST",
                         headers: {
                             "Accept": "application/json",
-                            "Content-type": "application/json"
+                            "Content-type": "application/json",
+                            "Keep-Alive": "timeout=240, max=100"
                         },
                         json: msg.payload
                     };
-                    d(`options=${JSON.stringify(options)}`);
+                    //d(`options=${JSON.stringify(options)}`);
+                    node.status({fill:"green", shape:"dot", text:"Sent to remote"});
+                    setTimeout( () => {
+                        node.status({});
+                    }, 1000);
                     request(url, options, (err, res, body) => {
                         if (!err && (res.statusCode < 200 || res.statusCode >= 300)) {
                             method = options["method"] || "GET";
@@ -48,12 +59,6 @@ module.exports = function(RED) {
                         if (err) {
                             node.status({fill:"red", shape:"ring", text:err.message});
                             msg.payload = undefined;
-                        }
-                        else {
-                            node.status({fill:"green", shape:"dot", text:"Sent to remote"});
-                            setTimeout( () => {
-                                node.status({});
-                            }, 1000);
                         }
 
                         msg.payload = body;
@@ -77,66 +82,38 @@ module.exports = function(RED) {
 
     /*
      * Tries to get (cpu, temp, mem) values from device
-     * - dummy : if dummy, returns directly the msg parameter; if not,
-     *           tries to read values (only rpi for the moment!)
+     * - dummy : if dummy, returns directly the msg parameter
      * - msg: values to return when dummy is true
+     *
+     * Returns { "mem": memValue, "temp": tempValue, "cpu": cpuValue}
      */
     function getValues(dummy, msg) {
 
         if (dummy) {
-            return Promise.resolve(msg);
+            d(`dummy mode`)
+            return Promise.resolve(
+                { "temp": msg.temp, "cpu": msg.cpu, "mem": msg.mem }
+            );
+        } else if (os.arch() == "arm64") {
+            d(`arm64 mode`)
+            return perf.getValuesRpi();
+        } else {
+            d(`x64 mode`)
+            return perf.getValuesX64();
         }
-        return getValuesRpi();
     }
 
-    function getValuesRpi() {
-        var cl = "vcgencmd measure_temp";
+    function toBeSentRemote(thresholds, values) {
+        curTemp = values.temp || 0;
+        curCpu = values.cpu || 0;
+        curMem = values.mem || 0;
 
-        return new Promise( (resolve, reject) => {
-            execp(cl).then(result => {
-                var str = result.stdout.replace("temp=", "").replace("'C", "");
-                d(`parsed temp=${str}`)
-                return resolve({ temp: parseFloat(str) });
-            }).catch( err => {
-                return reject(err)
-            });
-        })
-    }
-
-    function toBeSentRemote(config, msg) {
-        if (msg.link && msg.link == "remote") {
-            return true;
-        }
-        curTemp = msg.temp || 0;
-        curCpu = msg.cpu || 0;
-        curMem = msg.mem || 0;
-
-        maxTemp = config.temperature;
-        maxCpu = config.cpu;
-        maxMem = config.mem;
+        maxTemp = thresholds.temp;
+        maxCpu = thresholds.cpu;
+        maxMem = thresholds.mem;
 
         d(`cur(T,C,M)=${curTemp},${curCpu},${curMem} max(T,C,M)=${maxTemp},${maxCpu},${maxMem}`);
         return curTemp > maxTemp || curCpu > maxCpu || curMem > maxMem;
-    }
-
-    function execp(cmd, opts) {
-        opts || (opts = {});
-        d(`executing ${cmd}`)
-        return new Promise((resolve, reject) => {
-            exec(cmd, opts, (err, stdout, stderr) => {
-                if (err) {
-                    d(`execp: error=${err}`)
-                    return reject(err);
-                }
-                else {
-                    d(`execp: stdout=${stdout}`)
-                    return resolve({
-                        stdout: stdout,
-                        stderr: stderr
-                    });
-                }
-            });
-        });
     }
 
     RED.nodes.registerType("cloud-link", CloudLink);
